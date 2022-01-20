70a31cb6bacddc8f0e6a455c289ee09e18d0ab9a
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2022-01-20 22:24:31 +0100

    first (and only?) commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import eagle from "./eagle.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        className="App-header"
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: "1rem",
+        }}
+      >
+        <div>
+          <img src={eagle} alt="eagle" style={{ maxWidth: "100%" }} />
+          <h5>Gratulerer med dagen, gamle ørn!</h5>
+          <p>Håper du får en utroooooooolig fin bursdag.</p>
+          <p>Sjekk Vippsen din for gave.</p>
+          <p>
+            <small>(Bildet har jeg funnet selv på Google.)</small>
+          </p>
+        </div>
       </header>
     </div>
   );
